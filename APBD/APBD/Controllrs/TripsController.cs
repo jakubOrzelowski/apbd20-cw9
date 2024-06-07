9ddde52a342c2d01f24e2d478ab8ada0e2ea7034
jakuborzelowski <s27977@pjwstk.edu.pl>
2024-06-07 16:12:06 +0200

    dobry connection string

---
+++ APBD/APBD/Controllrs/TripsController.cs
@@ -0,0 +1,35 @@
+﻿using APBD.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD.Controllrs;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips
+            .Select(e => new
+            {
+                Name = e.Name,
+                Countries = e.IdCountries.Select(c => new
+                {
+                    Name = c.Name
+                })
+            })
+            .ToListAsync();
+
+        return Ok(trips);
+    }
+
+}
